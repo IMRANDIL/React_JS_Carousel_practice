aa992b8b3991e7adbd9b762db067d8ef6802611e
IMRANDIL <aliimranadil2@gmail.com>
2023-12-13 16:39:37 +0530

    fixed the styling bug as well

--- src/Data/ImageUri.ts
+++ src/Data/ImageUri.ts
@@ -18,7 +18,7 @@ export const imageUriArray = [
 
 export const fetchData = async()=>{
     const controller = new AbortController();
-    const jsonData = await fetch('https://picsum.photos/v2/list?page=2&limit=100', {
+    const jsonData = await fetch('https://picsum.photos/v2/list?page=2&limit=1000', {
         signal: controller.signal
     });
     const result = await jsonData.json();
